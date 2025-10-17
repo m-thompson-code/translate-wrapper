3b4e9f748c35f574cce4ef22ee5c7c89c93350b5
Mark Thompson <mthompson@bitovi.com>
2025-10-17 13:05:03 -0500

    added more demo information

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, inject, signal } from '@angular/core';
 import { TranslatePipe } from './translate/translate.pipe';
 import { TranslateService } from './translate/translate.service';
+import { startWith } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [
     TranslatePipe,
+    AsyncPipe,
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
@@ -14,6 +17,10 @@ export class App {
   protected readonly title = signal('multiple-jsons');
   private readonly translate = inject(TranslateService);
   lang: 'en' | 'de' = this.translate.getCurrentLang() as 'en' | 'de';
+  currentLanguage$ = this.translate.onLangChange.pipe(
+    startWith(this.translate.getCurrentLang()),
+  );
+  languages = this.translate.getLangs();
 
   constructor() {
     this.translate.onLangChange.subscribe((langChange) => {
